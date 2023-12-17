5a5440dd321389ca6a67078c5c9e00d771848bf1
Jaccha99 <92611658+Jaccha99@users.noreply.github.com>
2023-12-17 01:02:27 -0800

    Update StableMarriage.java

--- StableMarriage.java
+++ StableMarriage.java
@@ -1,13 +1,3 @@
-/*
-   Jacob Chalissery
-   10/27/2023
-   
-   This problem solves the classic "stable marriage" problem in which equal sized groups of men and women have ranked preferences of the opposite gender which they want to 
-   marry, and I pair them in the most "stable" way possible, by using a pattern in which a there is never a downgrade in preference. I use 2D Lists to store the preferences
-   for the men and women, a regular ArrayList for the names of men and women, a LinkedList to store the single men, which is what keeps the algorithm going, and a HashMap
-   to store the engaged couples.
-*/
-
 //import statements
 import java.util.*;
 import java.io.*;
